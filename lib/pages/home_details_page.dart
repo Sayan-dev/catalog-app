777d0fb840d5e58c964abda7614ce5e3d9652466
Sayanta <sayanta2702@gmail.com>
2024-05-28 16:33:19 +0530

    Added Design fixes

--- lib/pages/home_details_page.dart
+++ lib/pages/home_details_page.dart
@@ -12,9 +12,7 @@ class HomeDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Details",
-        ),
+        backgroundColor: MyThemeData.creamColor,
       ),
       backgroundColor: MyThemeData.creamColor,
       bottomNavigationBar: Container(
@@ -37,50 +35,40 @@ class HomeDetailPage extends StatelessWidget {
       ),
       body: SafeArea(
         child: Container(
-          child: Column(
-            children: [
-              Hero(tag: catalog.id, child: Image.network(catalog.image))
-                  .h32(context)
-                  .p16(),
-              VxArc(
-                  height: 30.0,
-                  arcType: VxArcType.convey,
-                  edge: VxEdge.top,
-                  child: Container(
-                    color: Colors.white,
-                    width: context.screenWidth,
-                    child: Column(
-                      children: [
-                        catalog.name.text.xl4
-                            .color(MyThemeData.darkBluishColor)
-                            .bold
-                            .make(),
-                        catalog.desc.text
-                            .textStyle(context.captionStyle)
-                            .make(),
-                        // ButtonBar(
-                        //   alignment: MainAxisAlignment.spaceBetween,
-                        //   buttonPadding: EdgeInsets.zero,
-                        //   children: [
-                        //     "\$${catalog.price}".text.bold.xl.make(),
-                        //     ElevatedButton(
-                        //       onPressed: () {},
-                        //       child: "Buy"
-                        //           .text
-                        //           .color(MyThemeData.creamColor)
-                        //           .make(),
-                        //       style: ButtonStyle(
-                        //           backgroundColor: WidgetStateProperty.all(
-                        //               MyThemeData.darkBluishColor),
-                        //           shape:
-                        //               WidgetStateProperty.all(StadiumBorder())),
-                        //     )
-                        //   ],
-                        // ).pOnly(top: 6)
-                      ],
-                    ).py64(),
-                  )).expand()
-            ],
+          child: Expanded(
+            child: Column(
+              children: [
+                Hero(tag: catalog.id, child: Image.network(catalog.image))
+                    .h32(context)
+                    .p16(),
+                VxArc(
+                    height: 30.0,
+                    arcType: VxArcType.convey,
+                    edge: VxEdge.top,
+                    child: Container(
+                      color: Colors.white,
+                      width: context.screenWidth,
+                      child: Column(
+                        children: [
+                          Text("Hello"),
+                          catalog.name.text.xl4
+                              .color(MyThemeData.darkBluishColor)
+                              .bold
+                              .make(),
+                          catalog.desc.text
+                              .textStyle(context.captionStyle)
+                              .make(),
+                          "Lorem ipsum dolor sit amet. Sed do eiusmod tempo aliqua nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate."
+                              .text
+                              .textStyle(context.captionStyle)
+                              .center
+                              .make()
+                              .py16()
+                        ],
+                      ).pLTRB(32, 64, 32, 40),
+                    ))
+              ],
+            ),
           ),
         ),
       ),
